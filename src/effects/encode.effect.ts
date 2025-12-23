e1dffe38e84fdbf46359b5536775d88db848d259
Adair Juneo <adair_juneo@hotmail.com>
2025-12-23 16:15:46 -0300

    feat: manual program functional, creating local _tmp folders.

--- src/effects/encode.effect.ts
+++ src/effects/encode.effect.ts
@@ -1,5 +1,7 @@
+import { randomUUID } from 'node:crypto';
+import path from 'node:path';
+import { FileSystem } from '@effect/platform';
 import { Effect, pipe } from 'effect';
-
 import { formatBytes } from '@/utils';
 
 interface EncodeParams {
@@ -7,24 +9,67 @@ interface EncodeParams {
   externalId: string;
 }
 
+const tmpPath = '_tmp';
+
 export const encode = (params: EncodeParams) =>
   Effect.gen(function* () {
     const { externalId, inputUrl } = params;
+    const encodeId = randomUUID(); // 🔴 TODO - Alterar aqui para o id que será retornado do in-memory-database.
+    const startTimeOfEncode = new Date().toJSON(); // 🔴 TODO - Alterar aqui para data de inicio gravada no in-memory-database.
 
-    yield* Effect.logInfo('All process encoded finished with successfully.');
+    /**
+     * 🟢 TODO - Criar pasta local no diretório _tmp para jogar os arquivos do encoding.
+     */
+    const fs = yield* FileSystem.FileSystem;
+    const baseDirectory = path.join(process.cwd(), tmpPath, encodeId);
+    yield* fs.makeDirectory(baseDirectory, { recursive: true }).pipe(
+      Effect.tap(() =>
+        Effect.logInfo('📁 Directory created successfully in _tmp folder.'),
+      ),
+      Effect.catchAll((error) =>
+        Effect.logError(`Failed to create directory ${encodeId}`, {
+          cause: error,
+        }),
+      ),
+    );
+
+    /**
+     * 🔴 TODO - Criar index no banco de dados em memória para armazenar dados sobre o encoding.
+     */
+    yield* Effect.logInfo(
+      `💾 Create index register in memory database with id ${encodeId}`,
+    );
+
+    /**
+     * 🟡 TODO - Send notification to SNS - "Encoding will start the processing."
+     */
+
+    yield* Effect.logInfo(
+      `🔄️ Encoding worker will start for ${encodeId} at ${startTimeOfEncode}`,
+    );
+    /**
+     * 🔴 TODO - Chamar o effect de encode de vídeo para iniciar o processo.
+     */
+
+    yield* Effect.logInfo('All worker encoding finished successfully.');
+
+    /**
+     * 🟡 TODO - Send notification to SNS - "Encoding finished successfully."
+     */
 
     const endTimeOfEncode = new Date().toISOString();
     const sizeInBytes = 10000;
-    const encodeId = 'fake-id-manual-test';
 
     yield* pipe(
-      Effect.logInfo(`🏁 Encoding worker are finished for ${encodeId} at ${endTimeOfEncode}`),
+      Effect.logInfo(
+        `🏁 Encoding worker are finished for ${encodeId} at ${endTimeOfEncode}`,
+      ),
       Effect.annotateLogs({
         inputUrl,
         encodeId,
         externalId,
         sizeInBytes: formatBytes(sizeInBytes),
         costInCents: 0,
-      })
+      }),
     );
   }).pipe(Effect.scoped, Effect.withSpan('/effect/encode'));
--- src/manual.ts
+++ src/manual.ts
@@ -7,7 +7,9 @@ const url =
   'https://pub-d161d9f2b7ce41e4a08dfd8c7f742dc7.r2.dev/raw/tests/bd2a56e2-ba7c-4aa5-af3b-e328dbb6abe0';
 const environment = env.NODE_ENV;
 
-console.info(`🚀 >>> Worker Initiated on ${environment}`);
+console.info(
+  `Worker Initiated on ${environment} ${new Date(Date.now()).toISOString()}`,
+);
 
 encode({
   inputUrl: url,
