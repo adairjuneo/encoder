abe784b64970532011b0b19f450509f1c8856a4e
Adair Juneo <adair_juneo@hotmail.com>
2025-12-30 12:43:50 -0300

    feat: update the file-system service with new method resolveDir and countFilesInDir.

--- src/services/file-system.service.ts
+++ src/services/file-system.service.ts
@@ -1,12 +1,19 @@
 import path from 'node:path';
 import { FileSystem } from '@effect/platform';
-import { Context, Effect, Layer, Stream } from 'effect';
+import { Context, Data, Effect, Layer, Stream } from 'effect';
+
+export class FileSystemServiceError extends Data.TaggedError(
+  'FileSystemServiceError',
+)<{
+  cause?: unknown;
+  message?: unknown;
+}> {}
 
 interface CreateDirReturn {
   path: string;
   asStream: (recursivePath: string) => Stream.Stream<Uint8Array, Error>;
   createSubDir: (subPath: string) => string;
-  getRecursivePath: (...paths: Array<string>) => string;
+  getRecursivePath: (...paths: string[]) => string;
 }
 
 export class FileSystemServiceContext extends Context.Tag('FileSystemService')<
@@ -14,7 +21,11 @@ export class FileSystemServiceContext extends Context.Tag('FileSystemService')<
   {
     readonly createDir: (
       rootPath: string,
+    ) => Effect.Effect<string, FileSystemServiceError, never>;
+    readonly resolveDir: (
+      rootPath: string,
     ) => Effect.Effect<CreateDirReturn, Error, never>;
+    readonly countFilesInDir: (rootPath: string) => number;
   }
 >() {}
 
@@ -36,57 +47,86 @@ export const FileSystemService = Layer.effect(
                 '📁 Directory created successfully in _tmp folder.',
               ),
             ),
-            Effect.map(() => ({
-              path: rootPath,
-              createSubDir: (subPath) => {
-                const subDirectoryPathToCreate = path.join(
-                  baseDirectory,
-                  rootPath,
-                  subPath,
-                );
-                fs.makeDirectory(subDirectoryPathToCreate, {
-                  recursive: true,
-                }).pipe(
-                  Effect.tap(() =>
-                    Effect.logInfo(
-                      '📁 Sub-Directory created successfully in root folder.',
-                    ),
-                  ),
-                );
-                return subDirectoryPathToCreate;
-              },
-              getRecursivePath: (...paths) => {
-                const recursivePath = path.join(
-                  directoryPathToCreate,
-                  ...paths,
-                );
-                return recursivePath;
-              },
-              asStream: (recursivePath) => {
-                const fullPath = path.join(
-                  baseDirectory,
-                  rootPath,
-                  recursivePath,
-                );
-
-                return Stream.fromEffect(
-                  Effect.gen(function* () {
-                    const fileStreamExists = yield* fs.exists(fullPath);
-                    if (!fileStreamExists) {
-                      return yield* Effect.fail(
-                        new Error(`File does not exists in ${fullPath}`),
-                      );
-                    }
-
-                    yield* Effect.logDebug(
-                      `Reading file as stream in ${fullPath}`,
-                    );
-                    return fullPath;
-                  }),
-                ).pipe(Stream.flatMap((streamPath) => fs.readFile(streamPath)));
-              },
-            })),
+            Effect.map(() => directoryPathToCreate),
+            Effect.catchAll((error) =>
+              Effect.fail(
+                new FileSystemServiceError({
+                  cause: error.cause,
+                  message: `Failed to create directory: ${error.message}`,
+                }),
+              ),
+            ),
           );
+      },
+      resolveDir: (rootPath: string) => {
+        const directoryPathToResolve = path.join(baseDirectory, rootPath);
+        return fs.exists(directoryPathToResolve).pipe(
+          Effect.catchAll((error) =>
+            Effect.fail(
+              new Error(
+                `Directory ${rootPath} does not exists in _tmp folder: ${error.message}`,
+              ),
+            ),
+          ),
+          Effect.map(() => ({
+            path: rootPath,
+            createSubDir: (subPath: string) => {
+              const subDirectoryPathToCreate = path.join(
+                directoryPathToResolve,
+                subPath,
+              );
+              fs.makeDirectory(subDirectoryPathToCreate, {
+                recursive: true,
+              }).pipe(
+                Effect.tap(() =>
+                  Effect.logInfo(
+                    '📁 Sub-Directory created successfully in root folder.',
+                  ),
+                ),
+              );
+              return subDirectoryPathToCreate;
+            },
+            getRecursivePath: (...paths: string[]) => {
+              const recursivePath = path.join(directoryPathToResolve, ...paths);
+              return recursivePath;
+            },
+            asStream: (recursivePath: string) => {
+              const fullPath = path.join(directoryPathToResolve, recursivePath);
+
+              return Stream.fromEffect(
+                Effect.gen(function* () {
+                  yield* fs.exists(fullPath).pipe(
+                    Effect.tap(() => {
+                      Effect.logDebug(`Reading file as stream in ${fullPath}`);
+                    }),
+                    Effect.catchAll((error) =>
+                      Effect.fail(
+                        new Error(
+                          `File does not exists in ${fullPath}: ${error.message}`,
+                        ),
+                      ),
+                    ),
+                  );
+
+                  return fullPath;
+                }),
+              ).pipe(Stream.flatMap((streamPath) => fs.readFile(streamPath)));
+            },
+          })),
+        );
+      },
+      countFilesInDir: (pathDir: string) => {
+        const directoryPathToWatch = path.join(baseDirectory, pathDir);
+        let countOfFiles = 0;
+        const files = fs.readDirectory(directoryPathToWatch, {
+          recursive: true,
+        });
+        files.pipe(
+          Effect.tap((filesInDir) => {
+            countOfFiles = filesInDir.length;
+          }),
+        );
+        return countOfFiles;
       },
     });
   }),
